e3b918f994f34ff26a42d268e03f3db39c712ed3
shay <shay.levin@develeap.com>
2024-04-21 16:35:51 +0300

    day 36

---
+++ day_36/main.py
@@ -0,0 +1,34 @@
+import requests
+from twilio.rest import Client
+import os
+
+api_active_members = "https://api.arboxapp.com/index.php/api/v2/users"
+headers = {
+    "apiKey": os.environ.get("APIKEY")
+}
+body_params = {"active": 1}
+number_total = 0
+number_whithout_mail = 0
+
+response = requests.get(api_active_members, headers=headers, params=body_params)
+response.raise_for_status()
+response_jason= response.json()
+
+for i in response_jason:
+    mail= i['email']
+    number_total += 1
+    if mail == None :
+        number_whithout_mail += 1
+print("have all the data")
+
+account_sid = 'AC06c13f5ccf2581297b896477d328012e'
+auth_token = 'e5377962b1861e267991c33f5dd4bd7e'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_="whatsapp:+14155238886",
+  body=f" monkeys have: {number_total} active claient and {number_whithout_mail} of them without mail. please pay attention",
+  to='whatsapp:+972547833192'
+)
+print(message.status)
+print("i send whatsupp")
