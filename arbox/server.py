436d505032c13157e98333de7624195905fd52b5
shay <shay.levin@develeap.com>
2024-06-13 15:46:41 +0300

    default_dates

--- arbox/server.py
+++ arbox/server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 import google_sheet.insert_data as insert_data
 from monthly_statistic.get_dates import get_default_start_end_dates
-from datetime import date
+from datetime import datetime
 
 
 app = Flask(__name__, template_folder='flask_server/templates', static_folder='flask_server/static')
@@ -12,8 +12,8 @@ app = Flask(__name__, template_folder='flask_server/templates', static_folder='f
 def generate_statistic():
     start_date = request.form.get("from")
     end_date = request.form.get("to")
-    start_date = date(year=int(start_date[:4]),month=int(start_date[5:7]), day=int(start_date[8:]))
-    end_date = date(year=int(end_date[:4]),month=int(end_date[5:7]), day=int(end_date[8:]))
+    start_date = datetime(year=int(start_date[:4]),month=int(start_date[5:7]), day=int(start_date[8:]))
+    end_date = datetime(year=int(end_date[:4]),month=int(end_date[5:7]), day=int(end_date[8:]))
     insert_data.main(start_date, end_date)
     return render_template("done_page.html")
 
